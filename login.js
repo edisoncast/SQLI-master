7242f3819249f9a03cd69856fb484c0ea60860f8
edisoncast <edisoncastrosanchez@gmail.com>
2019-11-26 12:50:15 -0500

    Incluir motor de plantillas en el backend para poder renderizar las vistas

--- login.js
+++ login.js
@@ -17,6 +17,7 @@ const connection = mysql.createConnection({
 //Inicialización y configuración del módulo de express.
 const app = express();
 
+
 app.use(session({
     secret: 'secret',
     cookie: { maxAge: 60000 },
@@ -25,39 +26,67 @@ app.use(session({
 }));
 
 
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.set('views', __dirname + '/views/');
+app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static("public"));
+//Esto no lo necesito pues estoy sirviendo el
+
 
 //Ruta para redireccionar al login
 app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname + '/public/login.html'));
+    response.render('login');
 });
 
-/*
-app.get('/style.css', function(req, res) {
-    res.sendFile(__dirname + "/" + "main.css");
-});*/
+app.get('/new', function(request, response) {
+    if (request.session.loggedin) {
+        const news = request.query.id;
+        if (news) {
+            let newsQuery = "SELECT * FROM `news` WHERE id = " + news;
+            connection.query(newsQuery, function(error, results, fields) {
+                if (error) {
+                    response.render('news');
+                    return;
+                }
+                if (results.length > 0) {
+                    let data = {
+                        'id': results[0].id,
+                        'title': results[0].title,
+                        'body': results[0].body,
+                        'Datetime': results[0].Datetime
+                    };
+                    response.render('news', { "data": data });
+                } else {
+                    response.send('Noticia no encontrada!').end();
+                }
+            });
+        } else {
+            response.render('news');
+        }
+    } else {
+        response.send('Por favor haga login para ver esta página!').end();
+    }
+});
 
 //Ruta para validar si un usuario puede ingresar
-
 
 app.post('/auth', function(request, response) {
     const email = request.body.email;
     const password = request.body.password;
     if (email && password) {
         const hash = crypto.pbkdf2Sync(password, "S3cureSalt", 100, 127, 'sha512').toString('hex');
-        let usernameQuery = "SELECT * FROM `users` WHERE email = '" + email + "' and password = '" + hash + "'";
-        console.log("Consulta", usernameQuery);
-
-        connection.query(usernameQuery, function(error, results, fields) {
+        connection.query('SELECT * FROM `users` WHERE email = ? AND password = ?', [email, hash], function(error, results, fields) {
             if (error) throw error;
             if (results.length > 0) {
                 request.session.loggedin = true;
                 request.session.email = email;
-                response.redirect('/public/news.html');
+                response.redirect('new');
             } else {
                 response.send('Usuario o password incorrecto!');
             }
@@ -67,15 +96,6 @@ app.post('/auth', function(request, response) {
         response.send('Usted debe ingresar un usuario y un password!');
         response.end();
     }
-});
-
-app.get('/news', function(request, response) {
-    if (request.session.loggedin) {
-        response.send('Bienvenido de nuevo, ' + request.session.email + '!');
-    } else {
-        response.send('Por favor haga login para ver esta página!');
-    }
-    response.end();
 });
 
 app.listen(3000, () => console.log('Servidor ejecutando en http://localhost:3000/'));
